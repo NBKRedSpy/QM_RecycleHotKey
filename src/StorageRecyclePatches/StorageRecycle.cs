88ccbf4a7750844944dc5ff3c3522b053385164c
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2025-12-05 18:42:30 -0500

    WIP

--- src/StorageRecyclePatches/StorageRecycle.cs
+++ src/StorageRecyclePatches/StorageRecycle.cs
@@ -1,6 +1,7 @@
 ﻿using MGSC;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,11 @@ namespace QM_RecycleHotKey.StorageRecyclePatches
         override public void Update()
         {
 
-            //Only the items storage.
-            if(!Input.GetKeyDown(Plugin.Config.RecycleCurrentPageKey))
+            //Do not include the cargo shuttle 
+            //TODO:  identify drop pod.
+            if(!Input.GetKeyDown(Plugin.Config.RecycleCurrentPageKey) ||
+                object.ReferenceEquals(Component._tabsView.FirstSelectedTab()._relatedWindow, 
+                    Component?._shuttleCargoView))
             {
                 return;
             }
@@ -28,10 +32,44 @@ namespace QM_RecycleHotKey.StorageRecyclePatches
 
             if(storage == null) return;
 
-
-
+            //Recycle all items in the storage.
+            DisassembleAllItems(storage);
         }
 
-        
+        private void DisassembleAllItems(ItemStorage storage)
+        {
+            //COPY WARNING: This is a logical copy of CorpseInspectWindow.DisassemblyAllItems()
+
+            //Using the the same variables as the decompiled version for easier comparison.
+
+            bool flag = false;  //any item was disassembled.
+            bool flag2 = false; //A weapon was disassembled.
+
+            List<BasePickupItem> list = new List<BasePickupItem>(storage.Items);
+
+            for (int i = 0; i < list.Count ; i++)
+            {
+                if (ItemInteractionSystem.CanDisassemble(list[i]))
+                {
+                    UI.Get<InventoryScreen>().DisassembleItem(list[i], -1, spendTurn: true, playSound: false);
+                    flag2 |= UI.Get<InventoryScreen>().TryUnloadWeapon(list[i], playSound: false);
+                    flag = true;
+                }
+            }
+            if (flag)
+            {
+                AudioClip clip = (flag2 ? SingletonMonoBehaviour<SoundsStorage>.Instance.AmmoReceived : SingletonMonoBehaviour<SoundsStorage>.Instance.EmptyAttack);
+                SingletonMonoBehaviour<SoundController>.Instance.PlayUiSound(clip);
+                PlayerInteractionSystem.EndPlayerTurn(Component._creatures, PlayerEndTurnContext.InventoryInteraction);
+                Component._creatures.Player.CreatureData.EffectsController.PropagateAction(PlayerActionHappened.HandAction);
+
+
+                //Sort and refresh the tabs.
+                storage.SortWithExpandByTypeAndName(Bootstrap._state.Get<SpaceTime>());
+                Component._tabsView.RefreshAllTabs();
+            }
+
+        }
     }
+
 }
